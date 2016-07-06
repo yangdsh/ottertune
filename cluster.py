4618c812d48c7753292b96a98417dc916eaf7de6
Dana Van Aken <dvanaken@cs.cmu.edu>
2016-07-05 20:31:37 -0400

    checkpoint

--- cluster.py
+++ cluster.py
@@ -4,51 +4,145 @@ Created on Jul 4, 2016
 @author: dva
 '''
 
+import os.path
 import numpy as np
 import random
 from sklearn.cluster import KMeans
+
+class KMeansHelper(object):
+    
+    def __init__(self, X, cluster_range=(1,21)):
+        self.cluster_range_ = cluster_range
+        self.cluster_map_ = {}
+        for k in range(*self.cluster_range_):
+            model = KMeansHelper.fit_model(X, k)
+            self.cluster_map_[k] = KMeansHelper.get_model_attributes(model)
+        
+        ks, logWks, logWkbs, sk = KMeansHelper.gap_statistic(X,
+                                                             self.cluster_map_,
+                                                             self.cluster_range_)
+        self.ks_      = ks
+        self.logWks_  = logWks
+        self.logWkbs_ = logWkbs
+        self.sk_      = sk
+        self.khats_   = KMeansHelper.compute_khats(self.ks_, self.logWks_,
+                                                   self.logWkbs_, self.sk_)
  
-def bounding_box(X):
-    xmin, xmax = min(X,key=lambda a:a[0])[0], max(X,key=lambda a:a[0])[0]
-    ymin, ymax = min(X,key=lambda a:a[1])[1], max(X,key=lambda a:a[1])[1]
-    return (xmin,xmax), (ymin,ymax)
+    @staticmethod
+    def bounding_box(X):
+        xmin, xmax = min(X,key=lambda a:a[0])[0], max(X,key=lambda a:a[0])[0]
+        ymin, ymax = min(X,key=lambda a:a[1])[1], max(X,key=lambda a:a[1])[1]
+        return (xmin,xmax), (ymin,ymax)
   
-def Wk(X, mu, cluster_labels):
-    K = len(mu)
-    norms = []
-    for i in range(K):
-        #for j in cluster_labels[i]:
-        c = X[cluster_labels == i]
-        norms.append(np.linalg.norm(mu[i]-c)**2/(2*len(c)))
-    return sum(norms)
-   
-def gap_statistic(X):
-    (xmin,xmax), (ymin,ymax) = bounding_box(X)
-    # Dispersion for real distribution
-    ks = range(1,21)
-    Wks = np.zeros(len(ks))
-    Wkbs = np.zeros(len(ks))
-    sk = np.zeros(len(ks))
-    for indk, k in enumerate(ks):
-        mu, cluster_labels = find_centers(X,k)
-        Wks[indk] = np.log(Wk(X, mu, cluster_labels))
-        # Create B reference datasets
-        B = 10
-        BWkbs = np.zeros(B)
-        for i in range(B):
-            Xb = []
-            for _ in range(len(X)):
-                Xb.append([random.uniform(xmin,xmax),
-                          random.uniform(ymin,ymax)])
-            Xb = np.array(Xb)
-            mu, cluster_labels = find_centers(Xb,k)
-            BWkbs[i] = np.log(Wk(Xb, mu, cluster_labels))
-        Wkbs[indk] = sum(BWkbs)/B
-        sk[indk] = np.sqrt(sum((BWkbs-Wkbs[indk])**2)/B)
-    sk = sk*np.sqrt(1+1/B)
-    return(ks, Wks, Wkbs, sk)
-   
-def find_centers(X, K):
-    kmeans = KMeans(K)
-    kmeans.fit(X)
-    return kmeans.cluster_centers_, kmeans.labels_
+    @staticmethod
+    def Wk(X, mu, cluster_labels):
+        K = len(mu)
+        return sum([np.linalg.norm(mu[i]-x)**2 for i in range(K) for x in X[cluster_labels == i]])
+#         K = len(mu)
+#         norms = []
+#         for i in range(K):
+#             c = X[cluster_labels == i]
+#             norms.append(np.linalg.norm(mu[i]-c)**2/(2*len(c)))
+#         return sum(norms)
+    
+    @staticmethod
+    def gap_statistic(X, cluster_map, cluster_range, B=10):
+        (xmin,xmax), (ymin,ymax) = KMeansHelper.bounding_box(X)
+        # Dispersion for real distribution
+        ks = np.arange(*cluster_range)
+        len_ks = ks.shape[0]
+        logWks = np.zeros(len_ks)
+        logWkbs = np.zeros(len_ks)
+        sk = np.zeros(len_ks)
+        for indk, k in enumerate(ks):
+            mu,cluster_labels,_ = cluster_map[k]
+            logWks[indk] = np.log(KMeansHelper.Wk(X, mu, cluster_labels))
+            # Create B reference datasets
+            logBWkbs = np.zeros(B)
+            for i in range(B):
+                Xb = []
+                for _ in range(X.shape[0]):
+                    Xb.append([random.uniform(xmin,xmax),
+                              random.uniform(ymin,ymax)])
+                Xb = np.array(Xb)
+                Xb_model = KMeansHelper.fit_model(Xb, k)
+                mu,cluster_labels,_ = KMeansHelper.get_model_attributes(Xb_model)
+                logBWkbs[i] = np.log(KMeansHelper.Wk(Xb, mu, cluster_labels))
+            logWkbs[indk] = sum(logBWkbs)/B
+            sk[indk] = np.sqrt(sum((logBWkbs-logWkbs[indk])**2)/B)
+        sk = sk*np.sqrt(1+1/B)
+        return (ks, logWks, logWkbs, sk)
+    
+    def plot(self, savedir):
+        inertias = np.empty(self.ks_.shape[0])
+        for i,(_,entry) in enumerate(sorted(self.cluster_map_.iteritems())):
+            inertias[i] = entry[2] 
+        KMeansHelper.plot_gaps(self.ks_, self.logWks_,
+                               self.logWkbs_, self.khats_,
+                               inertias, savedir)
+
+    @staticmethod
+    def get_model_attributes(model):
+        return model.cluster_centers_, model.labels_, model.inertia_
+    
+    @staticmethod
+    def fit_model(X, K):
+        kmeans = KMeans(K, n_init=20)
+        kmeans.fit(X)
+        return kmeans
+    
+    @staticmethod
+    def compute_khats(ks, logWks, logWkbs, sk):
+        len_ks = ks.shape[0]
+        khats = np.empty(len_ks - 1)
+        gs = logWkbs - logWks
+        gsks = gs - sk
+        for i in range(len_ks - 1):
+            khats[i] = gs[i] - gsks[i+1]
+        return khats
+    
+    @staticmethod
+    def plot_gaps(ks, logWks, logWkbs, khats, inertias, savedir):
+        import matplotlib.pyplot as plt
+        
+        # Plot the calculated gap
+        gaps = logWkbs - logWks
+        plt.figure()
+        plt.plot(ks, gaps)
+        plt.xlabel("# of clusters K")
+        plt.ylabel("Gap")
+        plt.savefig(os.path.join(savedir, "gap"))
+        plt.close()
+        
+        # Plot the sum of squares
+        plt.figure()
+        plt.plot(ks, np.exp(logWks))
+        plt.xlabel("# of clusters K")
+        plt.ylabel("per-cluster sum of squares")
+        plt.savefig(os.path.join(savedir, "sos"))
+        plt.close()
+        
+        # Plot the gap statistic
+        plt.figure()
+        plt.bar(ks[:-1], khats)
+        plt.xlabel("# of clusters K")
+        plt.ylabel("Gap(k)-(Gap(k+1)-s(k+1))")
+        plt.savefig(os.path.join(savedir, "gap_final"))
+        plt.close()
+        
+        # Plot the inertia
+        plt_clusters = np.empty(len(ks))
+        plt_inertia = np.empty(len(ks))
+        for i, (n_clusters, inertia) in enumerate(zip(ks,inertias)):
+            plt_clusters[i] = n_clusters
+            plt_inertia[i] = inertia
+         
+        fig = plt.figure()
+        plt.plot(plt_clusters, plt_inertia)
+        plt.xlabel("# Clusters")
+        plt.ylabel("Inertia")
+        plt.title("# Clusters vs. Inertia")
+        fig.canvas.set_window_title(os.path.basename(savedir))
+        plt.savefig(os.path.join(savedir, "inertia.pdf"), bbox_inches="tight")
+        plt.close()
+        
--- factor_analysis.py
+++ factor_analysis.py
@@ -6,22 +6,18 @@ Created on Jul 4, 2016
 
 import os.path
 import cPickle as pickle
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.distance import cdist
-from sklearn.cluster import KMeans
 from sklearn.decomposition import FactorAnalysis
 
-from .cluster import gap_statistic
+from .cluster import KMeansHelper
 from .matrix import Matrix
 from .preprocessing import Standardize
 from .util import stdev_zero
 from common.timeutil import stopwatch
 
-FACTOR_CUTOFF = 5
 
-
-def run_factor_analysis(paths, savedir):
+def run_factor_analysis(paths, savedir, factor_cutoff=5):
     import gc
 
     # Load matrices
@@ -52,61 +48,29 @@ def run_factor_analysis(paths, savedir):
         matrix.data = standardizer.fit_transform(matrix.data)
         
         # TODO: shuffle the data
+#         n_rows = matrix.data.shape[0]
+#         shuffle_indices = np.random.choice(n_rows, n_rows, replace=False)
+#         matrix.data = matrix.data[shuffle_indices]
 
     with stopwatch("fit factor analysis model"):
         # Fit the model to calculate the components
         fa = FactorAnalysis()
         fa.fit(matrix.data)
-    components = fa.components_[:FACTOR_CUTOFF].T
+    components = fa.components_[:factor_cutoff].T
  
-    ks, Wks, Wkbs, sk = gap_statistic(components)
-    G = []
-    
-    Wds = Wkbs - Wks
-    for i in range(len(ks) - 1):
-        G.append(Wds[i] - (Wds[i+1]-sk[i+1]))
-    G = np.array(G)
-    print G
-    return
-
-    # Run kmeans for different cluster sizes
-    cluster_info = {}
-    for n_clusters in range(2,21,2):
-        cluster_map = {}
-        with stopwatch("cluster for k={}".format(n_clusters)):
-            kmeans = KMeans(n_clusters)
-            kmeans.fit(components)
-            cluster_map['cluster_centers'] = kmeans.cluster_centers_
-            cluster_map['labels'] = kmeans.labels_
-            cluster_map['inertia'] = kmeans.inertia_
-        cluster_info[n_clusters] = cluster_map
-    
-    # Plot the inertia
-    plt_clusters = np.empty(len(cluster_info))
-    plt_inertia = np.empty(len(cluster_info))
-    for i, (n_clusters, entry) in enumerate(sorted(cluster_info.iteritems())):
-        plt_clusters[i] = n_clusters
-        plt_inertia[i] = entry['inertia']
-    
-    fig = plt.figure()
-    plt.plot(plt_clusters, plt_inertia)
-    plt.xlabel("# Clusters")
-    plt.ylabel("Inertia")
-    plt.title("# Clusters vs. Inertia")
-    fig.canvas.set_window_title(os.path.basename(savedir))
-    plt.savefig(os.path.join(savedir, "inertia.pdf"), bbox_inches="tight")
-    plt.close()
+    clusterer = KMeansHelper(components)
+    clusterer.plot(savedir)
     
     metric_clusters = {}
-    for n_clusters, entry in cluster_info.iteritems():
-        
+    for n_clusters, (cluster_centers,labels,_) in clusterer.cluster_map_.iteritems():
+         
         # For each cluster, calculate the distances of each metric from the
         # cluster center. We use the metric closest to the cluster center.
         mclusters = []
         for i in range(n_clusters):
-            metric_labels = matrix.columnlabels[entry['labels'] == i]
-            component_rows = components[entry['labels'] == i]
-            centroid = np.expand_dims(entry['cluster_centers'][i], axis=0)
+            metric_labels = matrix.columnlabels[labels == i]
+            component_rows = components[labels == i]
+            centroid = np.expand_dims(cluster_centers[i], axis=0)
             dists = np.empty(component_rows.shape[0])
             for j,row in enumerate(component_rows):
                 row = np.expand_dims(row, axis=0)
@@ -118,10 +82,9 @@ def run_factor_analysis(paths, savedir):
         metric_clusters[n_clusters] = mclusters
     
     with open(os.path.join(savedir, "metric_clusters.p"), "w") as f:
-        pickle.dump(metric_clusters, f)
+        pickle.dump(clusterer.cluster_map_, f)
     
     for n_clusters, memberships in sorted(metric_clusters.iteritems()):
-        
         cstr = ""
         for i,(cnum, lab, dist) in enumerate(memberships):
             assert i == cnum
