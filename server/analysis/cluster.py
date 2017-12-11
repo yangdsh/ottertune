50c2d4dcb2ae5b9a015a4b54a9e42174589b7b2b
Bohan Zhang <bohan@pku.edu.cn>
2017-12-11 01:09:34 -0500

    fix detk in cluster.py

--- server/analysis/cluster.py
+++ server/analysis/cluster.py
@@ -500,7 +500,8 @@ class DetK(KSelection):
                     else a(k - 1, Nd) + (1 - a(k - 1, Nd)) / 6
             Sks[i] = sum([np.linalg.norm(model.cluster_centers_[j] - c) ** 2
                           for j in range(K)
-                          for c in X[model.sample_labels_ == j]])
+                          for c in X[model.cluster_labels_ == j]])
+                          #for c in X[model.sample_labels_ == j]])
             if K == 1:
                 Fs[i] = 1
             elif Sks[i - 1] == 0:
