2c3834542a39714b52d4e02b5dd2f96246aa68a8
Bohan Zhang <bohan@pku.edu.cn>
2018-02-12 21:29:22 -0500

    fix workload mapping

--- server/website/website/tasks/async_tasks.py
+++ server/website/website/tasks/async_tasks.py
@@ -341,14 +341,16 @@ def map_workload(target_data):
     for workload_id, workload_entry in workload_data.iteritems():
         predictions = np.empty_like(y_target)
         X_workload = workload_entry['X_matrix']
+        X_scaled = X_scaler.transform(X_workload)
         y_workload = workload_entry['y_matrix']
-        for j, y_col in enumerate(y_workload.T):
+        y_scaled = y_scaler.transform(y_workload)
+        for j, y_col in enumerate(y_scaled.T):
             # Using this workload's data, train a Gaussian process model
             # and then predict the performance of each metric for each of
             # the knob configurations attempted so far by the target.
             y_col = y_col.reshape(-1, 1)
             model = GPR()
-            model.fit(X_workload, y_col, ridge=0.01)
+            model.fit(X_scaled, y_col, ridge=0.01)
             predictions[:, j] = model.predict(X_target).ypreds.ravel()
         # Bin each of the predicted metric columns by deciles and then
         # compute the score (i.e., distance) between the target workload
