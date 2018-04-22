362026dd4110eaed57c1867eeecc30a6029226e1
bohanjason <bohanzhang@cmu.edu>
2018-04-22 17:33:45 -0400

    set default values as min

--- server/website/website/tasks/async_tasks.py
+++ server/website/website/tasks/async_tasks.py
@@ -295,13 +295,25 @@ def configuration_recommendation(target_data):
     knobs_mem_catalog = {k.name: k for k in knobs_mem}
     mem_max = newest_result.workload.hardware.memory
     X_mem = np.zeros([1, X_scaled.shape[1]])
+    X_default = np.empty(X_scaled.shape[1])
 
+    # Get default knob values
+    for i, k_name in enumerate(X_columnlabels):
+        k = KnobCatalog.objects.filter(dbms=newest_result.session.dbms, name=k_name)[0]
+        X_default[i] = k.default
+
+    X_default_scaled = X_scaler.transform(X_default.reshape(1, X_default.shape[0]))[0]
     for i in range(X_scaled.shape[1]):
         col_min = X_scaled[:, i].min()
         col_max = X_scaled[:, i].max()
         if X_columnlabels[i] in knobs_mem_catalog:
             X_mem[0][i] = mem_max * 1024 * 1024 * 1024  # mem_max GB
             col_max = X_scaler.transform(X_mem)[0][i]
+
+        # Set min value to the default value
+        # FIXME: support multiple methods can be selected by users
+        col_min = X_default_scaled[i]
+
         X_min[i] = col_min
         X_max[i] = col_max
         X_samples[:, i] = np.random.rand(
